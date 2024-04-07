e13711a02e59bdc8aead6e8921bf533fed56a981
hasan.alhamwi <hasan.alhamwi@maqta.ae>
2024-04-07 17:31:56 +0400

    feat(styles): Add selectNewsLanguageComponentStyles

---
+++ src/styles/selectNewsLanguagesStyles.ts
@@ -0,0 +1,15 @@
+// Native import
+import {StyleSheet} from 'react-native';
+
+export const selectNewsLanguageComponentStyles =  StyleSheet.create({
+  languageItem: {
+    height: 50,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    borderRadius: 10,
+  },
+  languageContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
